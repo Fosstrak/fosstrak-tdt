4318adce74469790285a80c5f28cd359f9ae70e4
floerkem <floerkem@b63d296a-7662-4df4-3098-00f9c2c9103a>
2009-03-19 19:30:34 +0000

    Jochen Mader provided a patch that replaced castor with jaxb. This patch also added a method to load the xml configuration files as a resource and a constructor to add third party configuration files.

--- tdt/trunk/src/main/java/org/fosstrak/tdt/PrefixTree.java
+++ tdt/trunk/src/main/java/org/fosstrak/tdt/PrefixTree.java
@@ -19,109 +19,118 @@
  */
 
 package org.fosstrak.tdt;
-import java.util.*;
+
 import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
-
-/** Simple version of a Trie structure.
+/**
+ * Simple version of a Trie structure.
  * 
- *  See Wikipedia for information about Tries.
- *
- *  Note that this version assumes chars contain values between 0 and
- *  255 (i.e. UTF-8 or similar).
+ * See Wikipedia for information about Tries.
+ * 
+ * Note that this version assumes chars contain values between 0 and 255 (i.e.
+ * UTF-8 or similar).
+ * 
+ * @author Mark Harrison [University of Cambridge] - mark.harrison@cantab.net
+ * @author James Brusey
  */
-public class PrefixTree<T>
-{
+public class PrefixTree<T> {
 
-    private class PrefixNode {
-	private PrefixNode [] node;
+	private class PrefixNode {
+		private PrefixNode[] node;
 
-	// list is null if non-leaf
-	private List<T> list;
+		// list is null if non-leaf
+		private List<T> list;
 
-	public PrefixNode() {
-	    node = (PrefixNode[]) Array.newInstance(PrefixNode.class, 256);
+		public PrefixNode() {
+			node = (PrefixNode[]) Array.newInstance(PrefixNode.class, 256);
+		}
+
+		public PrefixNode[] getNode() {
+			return node;
+		}
+
+		public PrefixNode getNode(char c) {
+			return node[c];
+		}
+
+		public void setNode(char c, PrefixNode n) {
+			node[c] = n;
+		}
+
+		public List<T> getList() {
+			return list;
+		}
+
+		public boolean isMarked() {
+			return list != null;
+		}
+
+		public void add(T obj) {
+			if (list == null)
+				list = new ArrayList<T>();
+			list.add(obj);
+		}
+
 	}
 
-	public PrefixNode [] getNode() { return node; }
+	PrefixNode root = new PrefixNode();
 
-	public PrefixNode getNode(char c) { return node[c]; }
+	public void insert1(String s, T obj) {
+		// add a prefix to the tree
+		PrefixNode n = root;
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
 
-	public void setNode(char c, PrefixNode n) { 
-	    node[c] = n;
+			PrefixNode next = n.getNode(c);
+			if (next == null) {
+				next = new PrefixNode();
+				n.setNode(c, next);
+			}
+			n = next;
+		}
+		assert n != null;
+		n.add(obj);
 	}
 
-	public List<T> getList() { return list; }
-
-	public boolean isMarked() { return list != null; }
-
-	public void add(T obj) { 
-	    if (list == null) 
-		list = new ArrayList<T>();
-	    list.add(obj);
+	// recursive version
+	public void insert(String s, T obj) {
+		assert s != null : "Attempt to insert a null string into prefix tree";
+		myInsert(root, s, obj);
 	}
 
-    }
-     
-    PrefixNode root = new PrefixNode();
-
-    public void insert1(String s, T obj) {
-	// add a prefix to the tree 
-	PrefixNode n = root;
-	for (int i = 0; i < s.length(); i++) {
-	    char c = s.charAt(i);
-
-	    PrefixNode next = n.getNode(c);
-	    if (next == null) {
-		next = new PrefixNode();
-		n.setNode(c, next);
-	    }
-	    n = next;
+	private void myInsert(PrefixNode n, String s, T obj) {
+		if (s.equals(""))
+			n.add(obj);
+		else {
+			char c = s.charAt(0);
+			PrefixNode next = n.getNode(c);
+			if (next == null) {
+				next = new PrefixNode();
+				n.setNode(c, next);
+			}
+			myInsert(next, s.substring(1), obj);
+		}
 	}
-	assert n != null;
-	n.add(obj);
-    }
 
-    
-    // recursive version
-    public void insert(String s, T obj) {
-	assert s != null : "Attempt to insert a null string into prefix tree";
-	myInsert(root, s, obj);
-    }
-
-    private void myInsert(PrefixNode n, String s, T obj) { 
-	if (s.equals("")) 
-	    n.add(obj);
-	else {
-	    char c = s.charAt(0);
-	    PrefixNode next = n.getNode(c);
-	    if (next == null) {
-		next = new PrefixNode();
-		n.setNode(c, next);
-	    }
-	    myInsert(next, s.substring(1), obj);
+	public List<T> search(String s) {
+		return mySearch(root, s);
 	}
-    }
 
-
-    public List<T> search(String s) {
-	return mySearch(root, s);
-    }
-
-    private List<T> mySearch(PrefixNode n, String s) { 
-	if (s.equals(""))
-	    return n.getList();
-	else {
-	    char c = s.charAt(0);
-	    PrefixNode next = n.getNode(c);
-	    if (next == null) {
-		if (n.isMarked()) 
-		    return n.getList();
-		else
-		    return new ArrayList<T>();
-	    }
-	    else
-		return mySearch(next, s.substring(1));
+	private List<T> mySearch(PrefixNode n, String s) {
+		if (s.equals(""))
+			return n.getList();
+		else {
+			char c = s.charAt(0);
+			PrefixNode next = n.getNode(c);
+			if (next == null) {
+				if (n.isMarked())
+					return n.getList();
+				else
+					return new ArrayList<T>();
+			} else
+				return mySearch(next, s.substring(1));
+		}
 	}
-    }
 }
